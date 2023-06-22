31122f9271add6818e268eaa89c41291245b2614
Tomas Lingotti <tomaslingotti@gmail.com>
2023-06-22 14:39:06 -0300

    add zerolog

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"net/http"
 )
 
+const port = ":8080"
+
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
+	log.Info().Msgf("web app is running in port %s", port)
+	http.ListenAndServe(port, r)
 }
--- go.mod
+++ go.mod
@@ -11,5 +11,9 @@ require (
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/rs/zerolog v1.29.1 // indirect
+	golang.org/x/sys v0.9.0 // indirect
 )
